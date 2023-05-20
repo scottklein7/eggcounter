1c0482473049d6aeba5d8f2777809e63bfbf9a02
prescott-klein <prescott.c.klein.ctr@health.mil>
2023-05-20 15:45:55 -0400

    NAV mobile

--- Components/Nav.tsx
+++ Components/Nav.tsx
@@ -1,27 +1,49 @@
-import Link from "next/link";
-import Login from "./Login";
-import Image from "next/image";
+"use client"
+import { useEffect, useState } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import Login from './Login';
 
-function Nav() {
+const Nav = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    handleResize();
+
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
-    <nav className="bg-sky-200 p-4 flex items-center gap-5 text-lg font-extrabold text-emerald-600 justify-between">
+    <nav className="bg-sky-200 p-4 font-serif text-emerald-500 flex flex-col items-center md:flex-row md:justify-between">
       <div className="flex items-center gap-5">
         <Link href="/">
           <Image
             width="50"
             height="50"
+            className='rounded-xl'
             alt="chickenlogo"
             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbqXTHHoGknTXV4qrWQK5SQCdIh6gnxk9rXw&usqp=CAU"
           />
         </Link>
-
-        <Link className="hover:text-sky-400" href="/eggs">
-          See your egg data
-        </Link>
+        {!isMobile && (
+          <Link
+            className="hover:text-sky-400 text-xl"
+            href="/eggs"
+          >
+            See your egg data
+          </Link>
+        )}
       </div>
       <Login />
     </nav>
   );
-}
+};
 
 export default Nav;
